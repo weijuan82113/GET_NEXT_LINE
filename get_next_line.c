2176edd99bdab14b54b9ca2b9f5682d688a43fa3
wchen <wchen@42studen>
2022-10-09 02:07:14 +0900

    debug

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wchen <wchen@42studen>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 22:27:19 by wchen             #+#    #+#             */
-/*   Updated: 2022/10/08 23:29:53 by wchen            ###   ########.fr       */
+/*   Updated: 2022/10/09 02:06:40 by wchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,27 +46,46 @@ char	*get_next_line(int fd)
 	{
 		ret_chr = ft_save(line, find_return);
 		line = find_return + 1;
+		//free (line);
 		return (ret_chr);
 	}
 	buf = (void *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
 		return (NULL);
+	if (fd < 0 || BUFFER_SIZE < 0 || BUFFER_SIZE > SIZE_MAX)
+		return (NULL);
 	read_byte = read(fd, buf, BUFFER_SIZE);
 	if (!read_byte)
+	{
+		if (*line != '\0')
+		{
+			ret_chr = line;
+			line = "\0";
+			free (buf);
+			return (ret_chr);
+		}
 		return (NULL);
+	}
 	buf[BUFFER_SIZE] = '\0';
 	line = ft_strjoin(line, buf);
+	//printf("line is %s\n", line);
+	//free (line);
 	free (buf);
 	return (get_next_line(fd));
 }
 
 // int	main(void)
 // {
-// 	int fd;
-// 	char *print_char;
+// 	int 		fd;
+// 	char		*print_char;
+// 	ssize_t		read_byte;
+// 	char		buf[BUFFER_SIZE];
 
 // 	fd = 0;
-// 	fd = open("./hoge.txt", O_RDWR);
+// 	fd = open("./empty", O_RDWR);
+// 	read_byte = read(fd, buf, BUFFER_SIZE);
+// 	printf("read_byte is %zd \n", read_byte);
+// 	printf("fd is %d \n", fd);
 // 	print_char = get_next_line(fd);
 // 	while (print_char)
 // 	{
