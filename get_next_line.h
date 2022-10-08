31e4e9b31c30e4563fad84d4a9f9f1370d254408
wchen <wchen@42studen>
2022-10-08 23:34:04 +0900

    submit second

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,12 @@
 /*   By: wchen <wchen@42studen>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 00:15:08 by wchen             #+#    #+#             */
-/*   Updated: 2022/10/08 23:02:58 by wchen            ###   ########.fr       */
+/*   Updated: 2022/10/08 23:32:38 by wchen            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# ifdef	BUFFER_SIZE
-# else
-# define BUFFER_SIZE 1048575
-# endif
 
 # include <stddef.h>
 # include <stdlib.h>
@@ -27,8 +22,14 @@
 # include <sys/types.h>
 # include <sys/stat.h>
 
+# ifdef	BUFFER_SIZE
+# else
+#  define BUFFER_SIZE 1048575
+# endif
+
 size_t	ft_strlen(const char *c);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
 
 #endif
